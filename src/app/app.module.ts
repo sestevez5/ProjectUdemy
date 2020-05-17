b0ec898b80c60408b6d2f0b21e136dcc6669e425
Santi <sestevez5@gmail.com>
2020-05-17 13:17:21 +0100

    Main component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,19 @@
+import { HeaderComponent } from './components/header/header.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RecipesComponent } from './components/recipes/recipes.component';
+import { RecipesListComponent } from './components/recipes-list/recipes-list.component';
+import { RecipeDetailComponent } from './components/recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './components/recipe-item/recipe-item.component';
+import { ShoppingListComponent } from './components/shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './components/shopping-edit/shopping-edit.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, HeaderComponent, RecipesComponent, RecipesListComponent, RecipeDetailComponent, RecipeItemComponent, ShoppingListComponent, ShoppingEditComponent
   ],
   imports: [
     BrowserModule
