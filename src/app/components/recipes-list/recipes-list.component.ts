34e56f5ccefaac5fbe006f810fd070303d55c955
Santi <sestevez5@gmail.com>
2020-05-27 20:07:58 +0100

    subida

--- src/app/components/recipes-list/recipes-list.component.ts
+++ src/app/components/recipes-list/recipes-list.component.ts
@@ -9,7 +9,8 @@ import { Recipe } from '../../models/recipe.model';
 export class RecipesListComponent implements OnInit {
 
   recipes: Recipe[] = [
-    new Recipe('A test Recipe', 'This is simply a test', 'https://www.skinnytaste.com/wp-content/uploads/2009/02/turkey-meatloaf-8.jpg' )
+    new Recipe('A test Recipe', 'This is simply a test', 'https://ichef.bbci.co.uk/news/660/cpsprodpb/134E3/production/_105057097_a226d870-cc5f-4043-9f4b-d452b75cc280.jpg' ),
+    new Recipe('A test Recipe número 2', 'This is second a test', 'https://estaticos.miarevista.es/media/cache/1140x_thumb/uploads/images/gallery/59f6f86d5bafe8699bf7fdf3/segundosinterior.jpg' )
   ];
   constructor() { }
 
